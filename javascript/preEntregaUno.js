1fc87465a8c2b5e20fb1e411a13a66b4e6959df3
SastreMax <sastremaximiliano@gmail.com>
2024-05-22 22:36:46 -0300

    ganancias
    se modifica la funcion validar ingresos por ingresarGanancias

--- javascript/preEntregaUno.js
+++ javascript/preEntregaUno.js
@@ -9,6 +9,7 @@ let egresos = 0;
 let respuesta = "";
 let totalGastosProductos;
 let totalGastosServicios;
+let resultado;
 
 
 // funcion para validar el año ingresado //
@@ -36,7 +37,7 @@ function validarMes() {
 
 // funcion para ingresar gastos de forma individual por cada producto //
 
-function ingresarGastosProductos(cantidadGastosProductos) {
+function ingresarGastosProductos(GastosProductos) {
     let totalGastosProductos = 0;
     let cantidadProductos = parseInt(prompt("Ingrese la cantidad de productos comprados en el mes de " + mes));
     for (let i = 1; i < cantidadProductos + 1; i++) {
@@ -52,8 +53,8 @@ function ingresarGastosProductos(cantidadGastosProductos) {
 
 // funcion para ingresar gastos de forma individual por servicios de empleados //
 
-function ingresarGastosServicios() {
-    let totalGastos = 0;
+function ingresarGastosServicios(gastosServicios) {
+    let totalGastosServicios = 0;
     let cantidadEmpleados = parseInt(prompt("Ingrese la cantidad de empleados:"));
     for (let i = 0; i < cantidadEmpleados; i++) {
         let gasto = parseInt(prompt("Ingrese el sueldo para el empleado " + (i + 1) + ":"));
@@ -63,53 +64,28 @@ function ingresarGastosServicios() {
         }
         totalGastos += gasto;
     }
-    return totalGastos;
+    return totalGastosServicios;
 }
 
-// funcion para validar los egresos de las terapeutas //
+// funcion para registrar las ganancias de forma mensual //
 
-function validarIngresosEgresos(mes, año) {
-    let ingresos = parseInt(prompt("El monto de los ingresos en el mes de " + mes + ":"));
-    let egresos = parseInt(prompt("Ingrese el monto de los egresos en el mes de " + mes + ":"));
-    while (isNaN(ingresos) || isNaN(egresos) || ingresos < 0 || egresos < 0) {
-        if (isNaN(ingresos) || isNaN(egresos)) {
-            console.log("El monto ingresado no es un número válido.");
-        } else {
-            console.log("El monto ingresado no es válido (debe ser un número positivo).");
-        }
-        ingresos = parseInt(prompt("Ingrese el monto de los ingresos en el mes de " + mes + ":"));
-        egresos = parseInt(prompt("Ingrese el monto de los egresos en el mes de " + mes + ":"));
+function ingresarGanancias(mes, año) {
+    let ganancias = parseInt(prompt("Ingrese las ganancias del mes de " + mes + " del año " + año));
+    while (isNaN(ganancias) || ganancias < 0) {
+        console.log("El monto ingresado no es válido.");
+        ganancias = parseInt(prompt("Ingrese las ganancias del mes de " + mes + " del año " + año));
     }
-    return { ingresos, egresos };
+    return ganancias;
 }
-
-// ordenando las funciones //
-
-año = validarAño();
-mes = validarMes();
-totalGastosProductos = ingresarGastosProductos();
-totalGastosServicios = ingresarGastosServicios(); 
-resultado = validarIngresosEgresos(mes, año);
-
-
-// funcion para saber el saldo //
-
-function calcularSaldo(ingresos, egresos, mes) {
-    if (ingresos > egresos) {
-    console.log("El saldo en el mes de " + mes + " es positivo");
-    } else {
-    console.log("El saldo en el mes de " + mes + " es negativo");
-    }
-}
-
 
 // saldo  //
 
 mes = validarMes();
-let resultado = validarIngresosEgresos(mes);
-ingresos = resultado.ingresos;
-egresos = resultado.egresos;
-calcularSaldo(ingresos, egresos, mes);
+resultado = gastosServicios() + gastosProductos() + ingresos; 
+
+calcularSaldo(ingresos, egresos, mes, año);
+
+
 
 
 // suma de saldos //
@@ -124,3 +100,24 @@ do {
         calcularSaldo(ingresos, egresos, mes);
     }
 } while (respuesta === "si" || respuesta === "s");
+
+
+// ordenando las funciones //
+
+año = validarAño();
+mes = validarMes();
+totalGastosProductos = ingresarGastosProductos(cantidadGastosProductos);
+totalGastosServicios = ingresarGastosServicios(); 
+ingresos = ingresarGanancias(mes, año);
+egresos = totalGastosProductos + totalGastosServicios;
+
+
+// funcion para saber el saldo //
+
+function calcularSaldo(ingresos, egresos, mes, año) {
+    if (ingresos > egresos) {
+    console.log("El saldo en el mes de " + mes + " es positivo");
+    } else {
+    console.log("El saldo en el mes de " + mes + " es negativo");
+    }
+}
