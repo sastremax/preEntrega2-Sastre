aba1c51f5311d336fef608298f9bf9b06d833eba
SastreMax <sastremaximiliano@gmail.com>
2024-07-09 18:26:37 -0300

    se crean metodos: cargarApp, totalIngresos, TotalEgresoscargarCabecero
    Se crean estos metodos en app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,32 @@
 const INGRESOS = [
-    new INGRESOS("Sueldo", 3000000.00),
-    new INGRESOS("Alquileres", 1000000.00)
+    new Ingreso("Sueldo", 3000000.00),
+    new Ingreso("Alquileres", 1000000.00)
 ];
 const EGRESOS = [
-    new EGRESOS("Alquiler", 320000.00),
-    new EGRESOS("Comida", 250000.00)
+    new Egreso("Alquiler", 320000.00),
+    new Egreso("Comida", 250000.00)
 ];
+let cargarApp = () => {
+    cargarCabecero();
+}
+let totalIngresos = () => {
+    let totalIngreso = 0;
+    for(let ingreso of INGRESOS){
+        totalIngreso += ingreso.valor;
+    }
+    return totalIngreso;
+}
+
+let totalEgresos = () => {
+    let totalEgreso = 0;
+    for(let egreso of EGRESOS){
+        totalEgreso += egreso.valor;
+    }
+    return totalEgreso;
+}
+
+let cargarCabeceero = () =>{
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = totalEgresos()/totalIngresos();
+    document.getElementById("presupuesto").innerHTML = presupuesto;
+}
