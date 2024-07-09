6c1f67c18a9b17ffefcdbba2e4537ed8df6a3649
SastreMax <sastremaximiliano@gmail.com>
2024-07-09 17:57:12 -0300

    Se crean arrays en app.js
    Se crean dos arrays en app.js de ingresos y egresos

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+const INGRESOS = [
+    new INGRESOS("Sueldo", 3000000.00),
+    new INGRESOS("Alquileres", 1000000.00)
+];
+const EGRESOS = [
+    new EGRESOS("Alquiler", 320000.00),
+    new EGRESOS("Comida", 250000.00)
+];
